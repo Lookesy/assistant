5403686be3c2c536c32c9101594eb101a0604605
Lookesy <denis.titov2004@mail.ru>
2021-06-11 18:02:38 +0700

    update sort_bubble.py

--- sort_bubble.py
+++ sort_bubble.py
@@ -1,15 +1,13 @@
-from random import randint
- 
-N = 10
-a = []
-for i in range(N):
-    a.append(randint(1, 99))
-print(a)
- 
- 
-for i in range(N-1):
-    for j in range(N-i-1):
-        if a[j] > a[j+1]:
-            a[j], a[j+1] = a[j+1], a[j]
- 
-print(a)
+def bubble(array_list):
+    n = len(array_list)
+
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if array_list[j] > array_list[j + 1]:
+                array_list[j], array_list[j + 1] = array_list[j + 1], array_list[j]
+
+
+x = [1, 4, 2, 15, 21, 12, 64, -1, 2, 41, -21]
+
+bubble(x)
+print(x)
