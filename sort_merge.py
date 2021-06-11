d48f6acdff9579d457e26e1396d3e7a06c1c7a81
Lookesy <denis.titov2004@mail.ru>
2021-06-11 18:16:33 +0700

    Update sort_merge.py

--- sort_merge.py
+++ sort_merge.py
@@ -1,42 +1,44 @@
-def merge_sort(alist, start, end):
+def merge(alist):
+    def merge_sort(alist, start, end):
 
-    if end - start > 1:
-        mid = (start + end)//2
-        merge_sort(alist, start, mid)
-        merge_sort(alist, mid, end)
-        merge_list(alist, start, mid, end)
+        if end - start > 1:
+            mid = (start + end)//2
+            merge_sort(alist, start, mid)
+            merge_sort(alist, mid, end)
+            merge_list(alist, start, mid, end)
 
-def merge_list(alist, start, mid, end):
-    left = alist[start:mid]
-    right = alist[mid:end]
-    k = start
-    i = 0
-    j = 0
-    while (start + i < mid and mid + j < end):
-        if (left[i] <= right[j]):
-            alist[k] = left[i]
-            i = i + 1
+    def merge_list(alist, start, mid, end):
+        left = alist[start:mid]
+        right = alist[mid:end]
+        k = start
+        i = 0
+        j = 0
+        while (start + i < mid and mid + j < end):
+            if (left[i] <= right[j]):
+                alist[k] = left[i]
+                i = i + 1
+            else:
+                alist[k] = right[j]
+                j = j + 1
+            k = k + 1
+        if start + i < mid:
+            while k < end:
+                alist[k] = left[i]
+                i = i + 1
+                k = k + 1
         else:
-            alist[k] = right[j]
-            j = j + 1
-        k = k + 1
-    if start + i < mid:
-        while k < end:
-            alist[k] = left[i]
-            i = i + 1
-            k = k + 1
-    else:
-        while k < end:
-            alist[k] = right[j]
-            j = j + 1
-            k = k + 1
- 
- 
-alist = input('Введите список чисел: ').split()
-alist = [int(x) for x in alist]
-merge_sort(alist, 0, len(alist))
-print('Отсортированный список: ', end='')
-print(alist)
+            while k < end:
+                alist[k] = right[j]
+                j = j + 1
+                k = k + 1
 
-#Как и в случае с сортировкой пузырьком я просто за основу брал код с сайта,
-#поэтому и получилось точно так же. Сами методы я вроде как понял.
+    merge_sort(alist, 0, len(alist))
+    print('Отсортированный список: ', end='')
+    print(alist)
+
+
+x = [1, 2, 5, 2, 4, 53, 64, 12, -1, -5, -64, -85, 12]
+merge(x)
+
+
+#Я не уверен, что создавать одни функции в другой - это правильный вариант, но вроде работает, так что не жалуюсь.
