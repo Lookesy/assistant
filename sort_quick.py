7908a94cba8f5aadf31ad9ac6f6b930e00be47d6
Lookesy <denis.titov2004@mail.ru>
2021-06-12 16:58:27 +0700

    update sort_quick.py

--- sort_quick.py
+++ sort_quick.py
@@ -1,16 +1,30 @@
-import random
+def partition(nums, low, high):
+    pivot = nums[(low + high) // 2]
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while nums[i] < pivot:
+            i += 1
 
-def quicksort(nums):
-    if len(nums) <= 1:
-        return nums
-    else:
-        q = random.choise(nums)
-    l_nums = [n for n in nums if n <q]
+        j -= 1
+        while nums[j] > pivot:
+            j -= 1
 
-    e_nums = [q] * nums.count(q)
-    b_nums = [n for n in nums if h > q]
-    return quicksort(l_nums) + e_nums +quicksort(b_nums)
+        if i >= j:
+            return j
 
-x = [1, 4, 2, -1, -52, 64, 32, 43]
-quicksort(x)
-print(x)
+        nums[i], nums[j] = nums[j], nums[i]
+
+def quick(nums):
+    def _quick_sort(items, low, high):
+        if low < high:
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
+
+random_list_of_nums = [22, 5, 1, 18, 99]  
+quick(random_list_of_nums)
+print(random_list_of_nums) 
